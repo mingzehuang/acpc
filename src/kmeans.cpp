eaf2ab9675765b6efae2a4b4a668b94fb7144d1f
Huang <sharkmanhmz@tamu.edu>
2020-11-17 00:27:50 -0600

    Use my own version.

--- src/kmeans.cpp
+++ src/kmeans.cpp
@@ -1,70 +1,42 @@
-// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
-
-// we only include RcppArmadillo.h which pulls Rcpp.h in for us
-#include "RcppArmadillo.h"
-
-// via the depends attribute we tell Rcpp to create hooks for
-// RcppArmadillo so that the build process will know what to do
-//
+#include <RcppArmadillo.h>
 // [[Rcpp::depends(RcppArmadillo)]]
+using namespace Rcpp;
 
-// A - n x p matrix
-// B - m x p matrix
-arma::mat euclidDist(const arma::mat& A, const arma::mat& B){
-  arma::mat AB = - 2 * (A * arma::trans(B));
-  arma::mat dist = AB.each_row() + sum(arma::square(B), 1).t();
-  //returns the distances between each pair of rows of A and B
-  return(dist);
-}
-
-// X - n x p matrix of points
-// Y - n vector of labels for points in X
-// K - number of distinct labels in Y
-arma::mat newClusterCenters(const arma::mat& X, const arma::uvec& Y, int K) {
-  // M stores cluster centers
-  arma::mat M(K, X.n_cols, arma::fill::zeros);
-  // count stores the number of points in each cluster
-  arma::colvec count(K, arma::fill::zeros);
-  int n = X.n_rows;
-  for (arma::uword i = 0; i < n; ++i) {  // I think if you define i and calculate X.n_rows before the loop, it would be faster.
-    M.row(Y.at(i)) += X.row(i);
-    ++count(Y.at(i));
-  }
-  M.each_col() /= count;
-  // return M - K x p matrix of new cluster centers
-  return(M);
-}
-
-// X - n x p matrix
-// K - number of clusters
-// M - K x p cluster centers (always given)
-// numIter - maximal number of iterations
 // [[Rcpp::export]]
-arma::uvec MyKmeans_submission(const arma::mat& X, int K,
-                               const arma::mat& M, int numIter){
-  // All input is assumed to be correct
-  
-  // Initialize some parameters
+arma::ucolvec MyKmeans_c(const arma::mat& X, int K, const arma::mat& M, int numIter) {
   int n = X.n_rows;
   int p = X.n_cols;
-  arma::uvec Y(n); // to store cluster assignments
-  
-  // Initialize any additional parameters if needed
-  int counter = 0;
-  arma::mat Mprev = M + 1;
-  arma::mat Mnext = M;
-  
-  // For loop with kmeans algorithm
-  while (counter < numIter && any(vectorise(Mprev != Mnext))) {
-    Y = index_min(euclidDist(X, Mnext), 1);
-    ++counter;
-    Mprev = Mnext;
-    // compute new cluster centers based on new cluster assignments
-    Mnext = newClusterCenters(X, Y, K);
-    // compute new cluster assignments
-  }
-  
-  // Returns the vector of cluster assignments
-  return(Y + 1);
+  int KM = M.n_rows;
+  int pM = M.n_cols;
+  if (numIter < 1) {
+    throw std::runtime_error("Maximal number of iterations should be at least 1, whereas " + std::to_string(numIter) + "smaller than 1.");
+  } else if (K > n) {
+    throw std::runtime_error("Number of clusters K should not exceed number of data points n, whereas K = " + std::to_string(K) + "greater than rows of X =" + std::to_string(n));
+  } else if (K < 1) {
+    throw std::runtime_error("Number of clusters K should be at least 1, whereas K = " + std::to_string(K) + "smaller than 1.");
+  } else if (KM != K) {
+    throw std::runtime_error("Number of Cluster centers should be equal to number of clusters K, whereas M contains" + std::to_string(KM) + "rows, not equal to K =" + std::to_string(K));
+  } else if (pM != p) {
+    throw std::runtime_error("Dimensions of centers M should be equal to dimensions of data points X, whereas M contains" + std::to_string(pM) + "columns and X contains" + std::to_string(p) + "columns.");
+  };
+  arma::mat center_t(p, K);
+  arma::mat center_new_t = M.t();
+  arma::mat distance(n, K);
+  arma::ucolvec Y(n);
+  arma::ucolvec Y_r = arma::regspace< arma::ucolvec >(0, n - 1);
+  arma::mat Y_index(n, K);
+  arma::mat X_t = X.t();
+  int i = 0;
+  do {
+    center_t = center_new_t;
+    Y_index.zeros();
+    distance = 2 * X * center_t;
+    distance.each_row() -= arma::sum(arma::square(center_t), 0);
+    Y = arma::index_max(distance, 1);
+    Y_index(arma::sub2ind(arma::size(Y_index), (arma::join_rows(Y_r, Y)).t())).ones();
+    center_new_t = X_t * Y_index;
+    center_new_t.each_row() /= arma::sum(Y_index, 0);
+    i++;
+  } while((i < numIter) & (!(arma::approx_equal(center_new_t, center_t, "absdiff", 0))));;
+  return Y;
 }
-
