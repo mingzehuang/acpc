255d21e7363679b4281859a272465c44b49020ba
Huang <sharkmanhmz@tamu.edu>
2020-12-01 02:28:53 -0600

    I need input new X for extrapolation and return extrapolation label as output.

--- src/MultiLR.cpp
+++ src/MultiLR.cpp
@@ -29,7 +29,7 @@ arma::mat encode(const arma::uvec& y, const int& n, const int& K) {
 // lambda - ridge parameter, default 1
 // beta_init - p x K matrix of starting beta values (always supplied in right format)
 // [[Rcpp::export]]
-Rcpp::List LRMultiClass_c(const arma::mat& X, const arma::uvec& y, 
+Rcpp::List LRMultiClass_c(const arma::mat& X, const arma::uvec& y, const arma::mat& X_ext,
                           const arma::mat& beta_init,int numIter, 
                           double eta = 0.1, double lambda = 1){
   // All input is assumed to be correct
@@ -38,25 +38,21 @@ Rcpp::List LRMultiClass_c(const arma::mat& X, const arma::uvec& y,
   int K = max(y) + 1; // number of classes
   int p = X.n_cols;
   int n = X.n_rows;
-  
   // Initialize anything else that you may need
   arma::mat beta = beta_init; // to store betas and be able to change them if needed
   arma::vec objective(numIter + 1); // to store objective values
   arma::mat indmat = encode(y, n, K); // encode y using one-hot encoding method
   arma::uvec ind_c = arma::find(indmat == 0);  // get index for other elements
   arma::mat lambda_diag = arma::diagmat((lambda * arma::ones(p))); // diagonal lambda matrix
-  
   arma::mat eXb = arma::exp(X * beta); // Set matrix for exponential of Xb.
   arma::ucolvec y_pred = arma::index_max(eXb, 1); // Index for predicted y group.
   arma::colvec denominator_prob_beta = sum(eXb, 1); // Denominator for the probability given beta.
   arma::mat prob_beta = eXb.each_col() / denominator_prob_beta; // Probability given beta.
   arma::mat pick_prob_beta  = prob_beta; // Selected probability given beta.
-  
   arma::mat w; // w matrix for Hessian calculation.
   arma::mat gradient; // Gradient matrix.
   arma::mat X_t = X.t(); // Transposed X.
   arma::mat H; // Hessian matrix.
-  
   // Compute first objective with un-updated beta
   pick_prob_beta(ind_c).zeros();
   objective(0) = - sum(log(sum(pick_prob_beta, 1))) + (lambda / 2) * arma::accu(arma::square(beta));
@@ -70,7 +66,6 @@ Rcpp::List LRMultiClass_c(const arma::mat& X, const arma::uvec& y,
       H = X_t * (X.each_col() % w.col(j)) + lambda_diag;
       beta.col(j) = beta.col(j) - eta * arma::solve(H, gradient.col(j));
     };
-    
     // compute new objective
     eXb = arma::exp(X * beta);
     y_pred = arma::index_max(eXb, 1);
@@ -79,10 +74,8 @@ Rcpp::List LRMultiClass_c(const arma::mat& X, const arma::uvec& y,
     pick_prob_beta = prob_beta;
     pick_prob_beta(ind_c).zeros();
     objective(t + 1) = - sum(log(sum(pick_prob_beta, 1))) + (lambda / 2) * arma::accu(arma::square(beta));
-    
   }
-  
+    arma::uvec y_ext = arma::exp(X_ext * beta)
   // Create named list with betas and objective values
-  return Rcpp::List::create(Rcpp::Named("beta") = beta,
-                            Rcpp::Named("objective") = objective);
+  return y_ext;
 }
