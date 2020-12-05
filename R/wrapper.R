2bd419da08267ea9ba1bb33e3cdb823aea291e0d
Huang <sharkmanhmz@tamu.edu>
2020-12-05 06:22:59 -0600

    Roxygenize.

--- R/wrapper.R
+++ R/wrapper.R
@@ -1,5 +1,41 @@
 
-acpc <- function(X, center, r = 2, W = NULL , eps_r = 1e-4, MaxIter_r = 1e+5, gamma = 0.1, tau = 1, eps_s = 1e-4, MaxIter_s = 1e+3,  lambda = 1, MaxIter_k = 1e+3){
+#' Title
+#'
+#' @param X n * p matrix contains p features for n observations.
+#' @param cp K * 1 vector contains initial guess of cluster center points.
+#' @param r Scalar, number of principle components you want to generate, r<< p.
+#' @param W r * 1 vector, Weights on principle components.
+#' @param eps_r Scalar, tolerance for robust PCA.
+#' @param MaxIter_r Scalar, maximum iteration for robust PCA.
+#' @param gamma Scalar, relative weights between nuclear norm and sparsity penalties for robust PCA.
+#' @param tau Scalar, scale parameter for ADMM updates in robust PCA.
+#' @param eps_s Scalar, tolerance for sparSe PCA.
+#' @param MaxIter_s Scalar, maximum iteration for sparse PCA.
+#' @param lambda Scalar, sparsity penalty parameter for sparse PCA.
+#' @param MaxIter_k Scalar, maximum iteration for Kmeans.
+#'
+#' @return Label for clustering, Y; scores for clustering centers, center; loadings for principle components, V.
+#' @export
+#'
+#' @examples 
+#' # Create matrix M according to M = low rank + few large elements
+#' n = 100
+#' p = 40
+#' # Create rank 3 component
+#' set.seed(1234)
+#' out <- svd(matrix(rnorm(n*p), n, p), nu = 3, nv = 3)
+#' trueL <- out$u %*% diag(out$d[1:3]) %*% t(out$v)
+#' # Create sparse component
+#' trueS <- matrix(rt(n*p, df = 1), n, p)
+#' trueS[abs(trueS) < 2.5] <- 0
+#' sum(trueS !=0)
+#' # Create M by combining the above
+#' M <- trueL + trueS
+#' # Verify that M itself is not small rank and not even close to being rank 3
+#' svd(M)$d
+#' 
+
+acpc <- function(X, cp, r = 2, W = NULL , eps_r = 1e-4, MaxIter_r = 1e+5, gamma = 0.1, tau = 1, eps_s = 1e-4, MaxIter_s = 1e+3,  lambda = 1, MaxIter_k = 1e+3){
   X = as.matrix(X) # Convert X into matrix.
   n = nrow(X) # Compute rows of X.
   p = ncol(X) # Compute columns of X.
@@ -8,7 +44,7 @@ acpc <- function(X, center, r = 2, W = NULL , eps_r = 1e-4, MaxIter_r = 1e+5, ga
   spca = sparsePCA(L, eps_s, MaxIter_s, r, lambda) # Sparse PCA
   U = spca$U
   V = spca$V
-  M = U[center, , drop = F]
+  M = U[cp, , drop = F]
   if (missing(W) | is.null(W)) {
     W = rep(1/r, r)
   }
@@ -16,5 +52,5 @@ acpc <- function(X, center, r = 2, W = NULL , eps_r = 1e-4, MaxIter_r = 1e+5, ga
   Y = out$Y
   center = out$center
   # Return the class assignments
-  return(list(Y = Y, center= center, U = U, V = V, W = W))
+  return(list(Y = Y, center= center, V = V))
 }
