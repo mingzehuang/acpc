4ce648e8ac30edd7465823496b6efc92fdaf1f9d
Huang <sharkmanhmz@tamu.edu>
2020-12-07 00:07:14 -0600

    Fixed global variable assignment.

--- R/visualization.R
+++ R/visualization.R
@@ -1,14 +1,14 @@
 #' Interface for Area classification based on survey data.
 #'
-#' @param data_for_acpc Dataset for visualization.
+#' @param data_input Dataset for visualization.
 #'
 #' @return Run shiny app for output.
 #' @export
-visualization <- function(data_for_acpc = NULL) {
-  if (missing(data_for_acpc) | is.null(data_for_acpc)) {
-    data_for_acpc = readRDS(url("https://shiny.rstudio.com/tutorial/written-tutorial/lesson5/census-app/data/counties.rds", "rb"))
+visualization <- function(data_input = NULL) {
+  if (missing(data_input) | is.null(data_input)) {
+    data_input = readRDS(url("https://shiny.rstudio.com/tutorial/written-tutorial/lesson5/census-app/data/counties.rds", "rb"))
   }
-  assign("data_for_acpc", data_for_acpc)
+  assign("data_for_acpc", data_input, envir = .GlobalEnv)
   appDir <- system.file("shiny-examples", "myapp", package = "acpc")
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing `acpc`.", call. = FALSE)
