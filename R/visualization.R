6c95695a0cc169cd660682694230771a1b50d9cd
Huang <sharkmanhmz@tamu.edu>
2020-12-07 00:26:55 -0600

    Obviously, R do not allow you to change users' global environment through package.

--- R/visualization.R
+++ R/visualization.R
@@ -1,14 +1,8 @@
 #' Interface for Area classification based on survey data.
 #'
-#' @param data_input Dataset for visualization.
-#'
 #' @return Run shiny app for output.
 #' @export
-visualization <- function(data_input = NULL) {
-  if (missing(data_input) | is.null(data_input)) {
-    data_input = readRDS(url("https://shiny.rstudio.com/tutorial/written-tutorial/lesson5/census-app/data/counties.rds", "rb"))
-  }
-  assign("data_for_acpc", data_input, envir = .GlobalEnv)
+visualization <- function() {
   appDir <- system.file("shiny-examples", "myapp", package = "acpc")
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing `acpc`.", call. = FALSE)
