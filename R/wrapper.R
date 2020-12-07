765cdc61757c2f82bb7e842b5985d9679c79f118
Huang <sharkmanhmz@tamu.edu>
2020-12-07 04:37:06 -0600

    Add bunch of compatibility checks.

--- R/wrapper.R
+++ R/wrapper.R
@@ -4,7 +4,6 @@
 #' @param X n * (p + 1) matrix contains first column for name and the rest p numeric features for n observations.
 #' @param K Scalar, Number of clusters.
 #' @param r Scalar, number of principle components you want to generate, r<< p.
-#' @param cp K * 1 vector contains initial guess of cluster center points.
 #' @param eps_r Scalar, tolerance for robust PCA.
 #' @param MaxIter_r Scalar, maximum iteration for robust PCA.
 #' @param gamma Scalar, relative weights between nuclear norm and sparsity penalties for robust PCA.
@@ -37,27 +36,43 @@
 #' M <- cbind(data.frame(as.character(1:n), M))
 #' acpc(M, K)
 
-acpc <- function(X, K, r = 2, cp = NULL, eps_r = 1e-4, MaxIter_r = 1e+5, gamma = 0.1, tau = 1, eps_s = 1e-4, MaxIter_s = 1e+3,  lambda = 0.1, MaxIter_k = 1e+3){
+acpc <- function(X, K, r = 2, eps_r = 1e-4, MaxIter_r = 1e+5, gamma = 0.1, tau = 1, eps_s = 1e-4, MaxIter_s = 1e+3,  lambda = 0.1, MaxIter_k = 1e+3){
   X = na.omit(X) # Delete observations with missing value.
-  ID = X[ ,1] # Extract name ID for observations.
-  X = as.matrix(X[ ,-1]) # Extract features for observations.
+  ID = X[ , 1] # Extract name ID for observations.
+  X = X[ , -1] # Extract features for observations.
   featureID = colnames(X) # Etract ID for features.
-  n = nrow(X) # Compute rows of X.
-  p = ncol(X) # Compute columns of X.
+  X = as.matrix(X) # Convert features into matrix.
+  if (eps_r <= 0 | eps_s <= 0) {
+    stop(paste("Tolerance should be positive, whereas eps_r = ", eps_r, " or eps_s = ", eps_s, "not greater than 0."))
+  } else if (MaxIter_r < 10 | MaxIter_s < 10 | MaxIter_k < 10) {
+    stop(paste("To guarantee the possibility of convergence, please don't put maximum iteration number smaller than 10. ", MaxIter_r, " or ", MaxIter_s, " or ", MaxIter_k, "smaller than 10."))
+  } else if (gamma <= 0 | tau <= 0) {
+    stop(paste("Parameters gamma and tau should be positive for robust PCA, whereas gamma = ", gamma, " or tau = ", tau, "smaller or equal to 0."))
+  } else if (lambda < 0) {
+    stop(paste("Parameter lambda should be non-negative for sparse PCA, whereas lambda = ", lambda, "small than 0."))
+  } else if (is.null(featureID)) {
+    stop("Features should have column name as feature ID, otherwise you cannot interpret loadings.")
+  } else if (!(is.numeric(X))) {
+    stop("Features should be numeric to process robust sparse PCA and K-means.")
+  } else {
+    n = nrow(X) # Compute rows of X.
+    p = ncol(X) # Compute columns of X.
+  }
+  if (K <= 1 | K > n) {
+    stop(paste("Number of clusters should be integer at least 2, but not exceed number of observations, whereas clusters K = ", K, ", observations n = ", n, " after throwing missing value away."))
+  } else if (r < 1 | r > p) {
+    stop(paste("Number of principle conponents should integer be at least 1, but not exceed number of features, whereas principle components r = ", r, ", features p = ", p))
+  }
   X = scale(X)* sqrt(n/(n-1))   # Center and scale X
   L = robustPCA(X, eps_r, MaxIter_r, gamma, tau) # Robust PCA
   spca = sparsePCA(L, eps_s, MaxIter_s, r, lambda = 0.1) # Sparse PCA
   U = spca$U
   V = spca$V
-  rownames(V) = featureID
-  if (missing(cp) | is.null(cp)) {
-    cp = sample(n, K)
-  }
-  M = U[cp, , drop = F]
-  classification = Kmeans(U, M, MaxIter_k)
-  Y = classification$Y + 1
-  names(Y) = ID
-  center = classification$center
+  rownames(V) = featureID # Add feature ID onto loading matrix.
+  classification = Kmeans(U, U[sample(n, K), , drop = F], MaxIter_k) # K-means classification.
+  Y = classification$Y + 1 # Adjust cluster labels since C++ index from 0 instead of 1.
+  names(Y) = ID # Add observation ID onto score matrix.
+  center = classification$center # Extract scores of cluster centers.
   # Return the class assignments
   return(list(Y = Y, U = U, center = center, V = V))
 }
