18290a8534aff7594957fb19a8a03fdf1c57dcec
Huang <sharkmanhmz@tamu.edu>
2020-12-06 22:09:47 -0600

    Typo

--- R/visualization.R
+++ R/visualization.R
@@ -5,7 +5,7 @@
 #' @return Run shiny app for output.
 #' @export
 visualization <- function(censusdata) {
-  if (is.missing(cencusdata) | is.null(censusdata))
+  if (missing(cencusdata) | is.null(censusdata))
   censusdata = readRDS(url("https://shiny.rstudio.com/tutorial/written-tutorial/lesson5/census-app/data/counties.rds", "rb"))
   appDir <- system.file("shiny-examples", "myapp", package = "acpc")
   if (appDir == "") {
