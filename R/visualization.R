e02b389d605a380584d4c893e9725705d693edf3
Huang <sharkmanhmz@tamu.edu>
2020-12-06 22:02:38 -0600

    give example from online dataset.

--- R/visualization.R
+++ R/visualization.R
@@ -1,7 +1,9 @@
 #' Interface for Area classification based on survey data.
 #' @return Run shiny app for output.
 #' @export
-runExample <- function() {
+visualization <- function(censusdata) {
+  if (is.missing(cencusdata) | is.null(censusdata))
+  censusdata = readRDS(url("https://shiny.rstudio.com/tutorial/written-tutorial/lesson5/census-app/data/counties.rds", "rb"))
   appDir <- system.file("shiny-examples", "myapp", package = "acpc")
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing `acpc`.", call. = FALSE)
