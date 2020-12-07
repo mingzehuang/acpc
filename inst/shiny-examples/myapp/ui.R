d68e1711f39347cfbd097c6427909a76039d22f8
Huang <sharkmanhmz@tamu.edu>
2020-12-07 04:51:31 -0600

    Clustering to 1 class is meaningless.

--- inst/shiny-examples/myapp/ui.R
+++ inst/shiny-examples/myapp/ui.R
@@ -1,7 +1,7 @@
 pageWithSidebar(
   headerPanel('Area Classification by Robust Sparse PCA Kmeans Clustering for Two Principle Components'),
   sidebarPanel(
-    numericInput('clusters', 'Cluster count', 3, min = 1, max = 9),
+    numericInput('clusters', 'Cluster count', 3, min = 2, max = 9),
     hr(),
     numericInput('pc', 'Which Principle Component', 2, min = 1, max = 2)),
   mainPanel(
