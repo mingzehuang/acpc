003e7802f14eea7bbd308352939e58a7dfdb9ae5
Huang <sharkmanhmz@tamu.edu>
2020-12-06 23:44:57 -0600

    Unused argument.

--- R/visualization.R
+++ R/visualization.R
@@ -8,7 +8,7 @@ visualization <- function(data_for_acpc = NULL) {
   if (missing(data_for_acpc) | is.null(data_for_acpc)) {
     data_for_acpc = readRDS(url("https://shiny.rstudio.com/tutorial/written-tutorial/lesson5/census-app/data/counties.rds", "rb"))
   }
-  assign(data_for_acpc, data_for_acpc, evir = .GlobalEnv)
+  assign(data_for_acpc, data_for_acpc)
   appDir <- system.file("shiny-examples", "myapp", package = "acpc")
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing `acpc`.", call. = FALSE)
